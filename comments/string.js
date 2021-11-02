59b7c514e4fae83a97e78f625a1476160d8ac1d1
shariful islam <sharifulb07@gmail.com>
2021-11-02 20:16:03 +0600

    javascript event add here cool

---
+++ comments/string.js
@@ -0,0 +1,39 @@
+// let txt ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length=txt.length;
+// document.getElementById("fool").innerHTML=document.write(length);
+
+let str="Apple, Banana, Kiwi";
+// let part =str.substring(4,13);
+// document.getElementById("fool").innerHTML=document.write(part);
+// let part =str.slice(13);
+// document.getElementById("fool").innerHTML=document.write(part);
+
+
+let text="Please visit Microsoft";
+//let newText=text.replace("Microsoft", "AppleBigJayant")
+//let newText=text.replace(/Microsoft/i, "AppleBigJayant")
+//let newText=text.replace(/Microsoft/g, "AppleBigJayant")
+
+
+//let text1=text.toUpperCase();
+//let text1=text.toLowerCase();
+// let text1="Hi shariful islam ";
+// let text2="where are you ";
+// let text3="what are you doing now man  ";
+// let text4="what's up bro, be careful oh man cute dude  ";
+// let text7=text1.concat(" "+text2+" "+text3+" "+text4+" ");
+
+//let text1=text.toUpperCase();
+
+
+// let text1="Hi     hello world shariful    cool"
+// let text2=text1.trim();
+
+// let x="56"
+
+// document.getElementById("tool").innerHTML=x.padStart(6,0);
+// document.getElementById("fool").innerHTML=x.padEnd(6,0);
+
+var text ="HELLO WORLD ";
+
+document.getElementById("fool").innerHTML=text.charAt(2);
